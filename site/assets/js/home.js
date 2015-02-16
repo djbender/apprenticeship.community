e023def250d96d2abc7b27666ed58beb546d88e8
Zee Spencer <zee@zeespencer.com>
2015-02-15 22:28:19 -0800

    Add analytics for newsletter form and upper apply-to-join

--- site/assets/js/home.js
+++ site/assets/js/home.js
@@ -1,8 +1,22 @@
 $(document).ready(function() {
   AC.invite(AC.queryParam("invite-code"), $('.apply-to-join'), AC.URLBuilder.appendParam);
 
-  Track.cta(".applications-open .button", "Began application", {
-    color: "Blue"
+  Track.cta(".newsletter-form", "Signed up for Newsletter", {
+    position: "top-right"
+  , color: "Blue"
+  , category: "Engagement"
+  , pageSubject: $('body').attr('id')
+  });
+
+  Track.cta(".top applications-open .button", "Began application", {
+    position: "top-left"
+  , color: "Blue"
+  , category: "Applying"
+  , pageSubject: $('body').attr('id')
+  });
+  Track.cta(".bottom applications-open .button", "Began application", {
+    position: "bottom-center"
+  , color: "Blue"
   , category: "Applying"
   , pageSubject: $('body').attr('id')
   });
